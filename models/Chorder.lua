5d52f4afe76bd2d365993ace59adbc9bfab498e2
Pat Maddox <pat@patmaddox.com>
2016-09-16 20:25:44 -0600

    clean up spacing a bit

--- models/Chorder.lua
+++ models/Chorder.lua
@@ -11,8 +11,8 @@ arguments = {
       ["value"] = 57,
       ["properties"] = {
           ["min"] = 0,
-          ["max"] = 119,
           ["display_as"] = "note",
+          ["max"] = 119,
       },
       ["description"] = "",
   },
@@ -22,11 +22,11 @@ arguments = {
       ["linked"] = false,
       ["value"] = 2,
       ["properties"] = {
-          ["max"] = 2,
           ["items"] = {
               "major",
               "minor",
           },
+          ["max"] = 2,
           ["display_as"] = "popup",
           ["min"] = 1,
       },
@@ -38,12 +38,12 @@ arguments = {
       ["linked"] = false,
       ["value"] = 3,
       ["properties"] = {
-          ["max"] = 3,
           ["items"] = {
               "none",
               "major",
               "minor",
           },
+          ["max"] = 3,
           ["display_as"] = "popup",
           ["min"] = 1,
       },
@@ -55,8 +55,8 @@ arguments = {
       ["linked"] = false,
       ["value"] = 3,
       ["properties"] = {
-          ["max"] = 8,
           ["zero_based"] = false,
+          ["max"] = 8,
           ["display_as"] = "integer",
           ["min"] = 0,
       },
@@ -66,10 +66,10 @@ arguments = {
       ["locked"] = false,
       ["name"] = "chord.inversion",
       ["linked"] = false,
-      ["value"] = -1,
+      ["value"] = 0,
       ["properties"] = {
-          ["max"] = 3,
           ["zero_based"] = false,
+          ["max"] = 3,
           ["display_as"] = "integer",
           ["min"] = -3,
       },
@@ -81,47 +81,38 @@ presets = {
       ["chord.root"] = 60,
       ["name"] = "Cmaj7",
       ["chord.quality"] = 1,
-      ["ext.quality_7"] = 2,
-      ["bass.octave"] = 3,
       ["chord.inversion"] = 0,
+      ["bass.octave"] = 3,
+      ["ext.quality_7"] = 2,
   },
   {
       ["chord.root"] = 64.284,
       ["name"] = "Emin7 (-1i)",
       ["chord.quality"] = 2,
-      ["ext.quality_7"] = 3,
-      ["bass.octave"] = 3,
       ["chord.inversion"] = -1,
+      ["bass.octave"] = 3,
+      ["ext.quality_7"] = 3,
   },
 },
 data = {
-  ["noteFifth"] = [[return function()
-  return data.argsRoot() + data.interval_fifth
-end]],
+  ["interval_octave"] = "return 12",
   ["interval_minor_third"] = "return 3",
-  ["noteRoot"] = [[return function()
-  return data.argsRoot()
-end]],
-  ["noteSeventh"] = [[return function()
-  local seventh_quality = data.seventh_qualities[args.ext.quality_7]
-  local seventh_value
-  if seventh_quality == "major" then
-    seventh_value = data.interval_major_seventh
-  elseif seventh_quality == "minor" then
-    seventh_value = data.interval_minor_seventh
-  end
+  ["noteBass"] = [[return function()
+  local root_octave = math.floor(data.argsRoot()/ data.interval_octave)
+  local octave_diff = root_octave - args.bass.octave
 
-  return data.argsRoot() + seventh_value
+  return data.argsRoot() - (octave_diff * data.interval_octave)
 end]],
+  ["chord_qualities"] = "return { \"major\", \"minor\" }",
   ["interval_major_third"] = "return 4",
   ["interval_minor_seventh"] = "return 10",
-  ["invert"] = [[return function()
+  ["invert"] = [[return function(notes, inversion)
   local first = 1
-  local last = data.argsInversion()
+  local last = inversion
   local interval = data.interval_octave
   
   if data.argsInversion() < 0 then
-    first = #notes + data.argsInversion()
+    first = #notes + inversion
     last = #notes
     interval = -data.interval_octave
   end
@@ -133,17 +124,16 @@ end]],
   ["root"] = [[return function()
   return data.noteRoot()
 end]],
-  ["noteBass"] = [[return function()
-  local root_octave = math.floor(data.argsRoot()/ data.interval_octave)
-  local octave_diff = root_octave - args.bass.octave
-
-  return data.argsRoot() - (octave_diff * data.interval_octave)
+  ["noteRoot"] = [[return function()
+  return data.argsRoot()
 end]],
   ["interval_fifth"] = "return 7",
   ["argsInversion"] = [[return function()
   return tonumber(args.chord.inversion)
 end]],
-  ["interval_octave"] = "return 12",
+  ["noteFifth"] = [[return function()
+  return data.argsRoot() + data.interval_fifth
+end]],
   ["argsRoot"] = [[return function()
   return math.floor(args.chord.root)
 end]],
@@ -160,7 +150,17 @@ end]],
 
   return data.argsRoot() + third_value
 end]],
-  ["chord_qualities"] = "return { \"major\", \"minor\" }",
+  ["noteSeventh"] = [[return function()
+  local seventh_quality = data.seventh_qualities[args.ext.quality_7]
+  local seventh_value
+  if seventh_quality == "major" then
+    seventh_value = data.interval_major_seventh
+  elseif seventh_quality == "minor" then
+    seventh_value = data.interval_minor_seventh
+  end
+
+  return data.argsRoot() + seventh_value
+end]],
   ["interval_major_seventh"] = "return 11",
 },
 events = {
@@ -171,24 +171,22 @@ options = {
 callback = [[
 xline = table.rcopy(EMPTY_XLINE)
 
-
-notes = {
+local notes = {
   data.noteRoot(),
   data.noteThird(),
   data.noteFifth(),
   data.noteSeventh(),
 }
+data.invert(notes, data.argsInversion())
 
 notes[#notes+1] = data.noteBass()
-data.invert()
 
 table.sort(notes)
-rprint(notes)
-
-print("success!")
 
 for i=1,#notes do
   xline.note_columns[i].note_value = notes[i]
 end
+
+print("success!")
 ]],
 }
